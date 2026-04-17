292684c22c8be542dcdb651495afa1e4a6393e57
Minjae Cho <whalswo117@gmail.com>
2026-04-17 17:09:03 -0500

    asd

--- test.bash
+++ test.bash
@@ -10,7 +10,7 @@ nohup python3 main.py --project ablation_irpo --env-name pointmaze-v4 --algo-nam
 nohup python3 main.py --project ablation_irpo --env-name pointmaze-v4 --algo-name irpo --num-runs 10 --aggregation-method uniform --gpu-idx 1 &  # uniform -> argmax
 nohup python3 main.py --project ablation_irpo --env-name pointmaze-v4 --algo-name irpo --num-runs 10 --aggregation-method argmax --gpu-idx 1 &  # argmax
 
-nohup python3 main.py --project ablation_irpo --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --aggregation-method argmax --noise-std 0.0 --gpu-idx 2 &  # argmax
-nohup python3 main.py --project ablation_irpo --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --aggregation-method argmax --noise-std 0.3 --gpu-idx 2 &  # argmax
-nohup python3 main.py --project ablation_irpo --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --aggregation-method argmax --noise-std 0.5 --gpu-idx 2 &  # argmax
-nohup python3 main.py --project ablation_irpo --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --aggregation-method argmax --noise-std 1.0 --gpu-idx 2 &  # argmax
+nohup python3 main.py --project ablation_irpo --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --aggregation-method softmax --noise-std 0.0 --gpu-idx 2 &  # argmax
+nohup python3 main.py --project ablation_irpo --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --aggregation-method softmax --noise-std 0.3 --gpu-idx 2 &  # argmax
+nohup python3 main.py --project ablation_irpo --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --aggregation-method softmax --noise-std 0.5 --gpu-idx 2 &  # argmax
+nohup python3 main.py --project ablation_irpo --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --aggregation-method softmax --noise-std 1.0 --gpu-idx 2 &  # argmax
