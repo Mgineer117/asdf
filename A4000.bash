fcf3c2f04defb6f355aec1b217f92d31f09ff352
Minjae Cho <whalswo117@gmail.com>
2026-04-21 17:45:25 -0500

    asdwqe

--- A4000.bash
+++ A4000.bash
@@ -2,10 +2,10 @@
 # TRAP REWARD
 # TRAIN FOR ANTMAZE
 
-nohup python3 main.py --project trap_reward --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --num-exp-updates 5 --rendering --gpu-idx 0 &
-nohup python3 main.py --project trap_reward --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --num-exp-updates 10 --rendering --gpu-idx 0 &
-nohup python3 main.py --project trap_reward --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --num-exp-updates 15 --rendering --gpu-idx 0 &
-nohup python3 main.py --project trap_reward --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --num-exp-updates 20 --rendering --gpu-idx 0 &
+nohup python3 main.py --project trap_reward --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --num-exp-updates 5 --rendering --gpu-idx 2 &
+nohup python3 main.py --project trap_reward --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --num-exp-updates 10 --rendering --gpu-idx 2 &
+nohup python3 main.py --project trap_reward --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --num-exp-updates 15 --rendering --gpu-idx 2 &
+nohup python3 main.py --project trap_reward --env-name fourrooms-v2 --algo-name irpo --num-runs 10 --num-exp-updates 20 --rendering --gpu-idx 2 &
 
 nohup python3 train_allo.py --project allo --env-name antmaze-v1 --num-runs 10 --gpu-idx 1 &
 nohup python3 train_allo.py --project allo --env-name antmaze-v2 --num-runs 10 --gpu-idx 1 &
--- Europa.bash
+++ Europa.bash
@@ -1,7 +1,7 @@
 # EUROPA
-nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method softmax --beta 0.0 --gpu-idx 2 &  # softmax -> argnax
-nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method softmax --beta 0.99 --gpu-idx 2 &  # softmax -> argnax
-nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method uniform --beta 0.0 --gpu-idx 2 &  # uniform -> argmax
-nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method uniform --beta 0.99 --gpu-idx 3 &  # uniform -> argmax
-nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method argmax --beta 0.0 --gpu-idx 3 &  # argmax
-nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method argmax --beta 0.99 --gpu-idx 3 &  # argmax
+nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method softmax --beta 0.0 --gpu-idx 1 &  # softmax -> argnax
+nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method softmax --beta 0.99 --gpu-idx 1 &  # softmax -> argnax
+nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method uniform --beta 0.0 --gpu-idx 1 &  # uniform -> argmax
+nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method uniform --beta 0.99 --gpu-idx 2 &  # uniform -> argmax
+nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method argmax --beta 0.0 --gpu-idx 2 &  # argmax
+nohup python3 main.py --project ablation_irpo --env-name pointmaze-v1 --algo-name irpo --num-runs 10 --aggregation-method argmax --beta 0.99 --gpu-idx 2 &  # argmax
