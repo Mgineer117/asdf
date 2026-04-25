bfd47407c49a0274c1e282294374593d78fecd6d
Minjae Cho <whalswo117@gmail.com>
2026-04-25 17:37:05 -0500

    asdasd

--- commands/run_amidar.bash
+++
@@ -1,5 +0,0 @@
-for script in commands/amidar/*.sbatch; do
-    echo "Submitting $script..."
-    sbatch "$script"
-    sleep 2
-done
--- commands/run_pacman.bash
+++
@@ -1,5 +0,0 @@
-for script in commands/pacman/*.sbatch; do
-    echo "Submitting $script..."
-    sbatch "$script"
-    sleep 2
-done
